cca75b5d01c16dc31e98f85c76438bf01f90f71b
Stephen Molyneaux <stephen@molyneaux.ca>
2021-11-13 10:15:34 -0800

    Configure TailwindCSS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,12 @@
     <title>SHAM</title>
 </svelte:head>
 
-<h1>Welcome to SHAM</h1>
+<h1 class="text-4xl">Welcome to SHAM</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <script>
+    import '../styles/tailwind.css'
+
     let name = 'world';
 </script>
 
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/**/*.{svelte,ts,js,html}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
