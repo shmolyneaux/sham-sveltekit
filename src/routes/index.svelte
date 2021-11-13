aef425c557fd8136ca51a453c15acfb5af0b81be
Stephen Molyneaux <stephen@molyneaux.ca>
2021-11-12 21:48:53 -0800

    Update index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<svelte:head>
+    <title>SHAM</title>
+</svelte:head>
+
+<h1>Welcome to SHAM</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
